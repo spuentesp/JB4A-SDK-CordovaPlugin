eb7e4777b3efb1eaf4a8e76c3394550300d25533
Jesse Boles <jboles@exacttarget.com>
2014-04-24 12:06:04 -0400

    added cordova jar that was missing

--- www/push.js
+++ www/push.js
@@ -75,6 +75,50 @@ channel.waitForInitialization('onCordovaInfoReady');
     Test: function(successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "SdkWrapper", "test", []);
     }
+    register: function(successCallback, errorCallback, accessToken, appID)
+    {
+        
+    }
+    //returns boolean with whether device notifications are active
+    isPushEnabled: function(SuccessCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "isPushEnabled", []);
+    }
+    //returns the uniqe device identifier
+    safeDeviceIdentifier: function(SuccessCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "safeDeviceIdentifier", []);
+    }
+    //get the current subscriber key
+    getSubscriberKey: function(SuccessCallback, errorCallback, subscriberKey) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //set the current subscriber key
+    setSubscriberKey: function(SuccessCallback, errorCallback,subscriberKey, value) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    // add a tag 
+    addTag: function(SuccessCallback, errorCallback, tag, value) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //remove a tag
+    removeTag: function(SuccessCallback, errorCallback, tag) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //return all the tags
+    allTags: function(SuccessCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //add an attribute and set its value
+    addAttribute: function(SuccessCallback, errorCallback, name, var value) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //remove an attribute
+    removeAttribute: function(SuccessCallback, errorCallback, name, var value) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
+    //returns all attributes
+    allAttributes: function(SuccessCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
+    }
  }
 
 module.exports = ETPush;
