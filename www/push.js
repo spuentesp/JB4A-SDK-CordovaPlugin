6f83f9d29f4741df31633f7f0d560e95ba04a535
Jesse Boles <jboles@exacttarget.com>
2014-04-25 09:34:58 -0400

    working plugin code callable in another file

--- www/push.js
+++ www/push.js
@@ -1,69 +1,8 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
+var exec = require('cordova/exec');
 
-/**
- * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
- * phone, etc.
- * @constructor
- */
-/*function ETPush() {
-    this.available = false;
-    this.platform = null;
-    this.version = null;
-    this.uuid = null;
-    this.cordova = null;
-    this.model = null;
-
-    var me = this;
-
-    channel.onCordovaReady.subscribe(function() {
-        me.getInfo(function(info) {
-            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-            //TODO: CB-5105 native implementations should not return info.cordova
-            var buildLabel = cordova.version;
-            me.available = true;
-            me.platform = info.platform;
-            me.version = info.version;
-            me.uuid = info.uuid;
-            me.cordova = buildLabel;
-            me.model = info.model;
-            channel.onCordovaInfoReady.fire();
-        },function(e) {
-            me.available = false;
-            utils.alert("[ERROR] Error initializing Cordova: " + e);
-        });
-    });
-}
-*/
-
-/**
- * Get device info
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-
- var ETPush =( function() {
-    var etpush = {},
-    Test: function(successCallback, errorCallback) {
+ module.exports = {
+ Test: function(successCallback, errorCallback) {
+    console.log("Testing etpush");
         cordova.exec(successCallback, errorCallback, "SdkWrapper", "test", []);
     },
     register: function(successCallback, errorCallback, accessToken, appID)
@@ -110,5 +49,4 @@
     allAttributes: function(SuccessCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "SdkWrapper", "getSubscriberKey", []);
     },
- }());
-module.exports = ETPush;
+};
